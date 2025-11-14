26242d6796a738434a04f5c4168306358bfbd40d
uwuclxdy <filip.zavrnik@gmail.com>
2025-11-14 20:59:33 +0100

    refactor tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,2 +1,7 @@
 mod common_tests;
 mod helpers_tests;
+mod ollama_endpoints_tests;
+mod lmstudio_api_tests;
+mod parameter_mapping_tests;
+mod model_resolution_tests;
+mod response_transformation_tests;
