fa4295bff341bad2e752cdbfe06254b6da891448
cloudy <37777261+uwuclxdy@users.noreply.github.com>
2026-05-22 01:20:37 +0200

    style(update): wrap long line per rustfmt

--- src/update.rs
+++ src/update.rs
@@ -8,7 +8,8 @@ const CHECK_INTERVAL: Duration = Duration::from_secs(60 * 60 * 24);
 pub fn check_for_update() {
     let current = crate::VERSION;
     tokio::task::spawn_blocking(move || {
-        let informer = update_informer::new(registry::GitHub, REPO, current).interval(CHECK_INTERVAL);
+        let informer =
+            update_informer::new(registry::GitHub, REPO, current).interval(CHECK_INTERVAL);
         match informer.check_version() {
             Ok(Some(latest)) => {
                 let latest = latest.to_string();
